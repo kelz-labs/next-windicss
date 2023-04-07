f7618ac8a5b01491dcda5bf638651b3d18911df7
haikelz <haikel.2017th@gmail.com>
2023-04-08 04:37:47 +0700

    chore: init

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+export default function HomePage() {
+  return (
+    <main className="w-full flex justify-center min-h-screen items-center">
+      <section className="max-w-5xl flex justify-center items-center w-full">
+        <div className="border-4 hover:border-6 cursor-pointer border-black p-4">
+          <h1 className="heading">Hello from Windi CSS</h1>
+        </div>
+      </section>
+    </main>
+  );
+}
